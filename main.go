c985d5b05a94bcec592e19470cbd11cad52c690e
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-07 21:55:20 +0700

    read streaming the file for each 8 bytes

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
-	println("I hope I get the job!")
+	f, err := os.Open("./messages.txt")
+	if err != nil {
+		fmt.Println("Cannot read messages.txt file on root: ", err.Error())
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	readBuffer := make([]byte, 8)
+
+	for {
+		_, err := f.Read(readBuffer)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("cannot read streaming file: ", err.Error())
+		}
+
+		fmt.Printf("read: %s\n", string(readBuffer))
+	}
 }
