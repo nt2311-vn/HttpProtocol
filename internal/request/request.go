929eab89a6938c14cd0393b6273b2d8955e374c9
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-08 12:46:55 +0700

    write function and test cases

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,16 @@
 package request
+
+import "io"
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+}
