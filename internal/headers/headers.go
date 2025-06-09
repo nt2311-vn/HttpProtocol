8fd3f49e32ddf8e1dc384483431583878c963606
nt2311-vn <nguyentoan231196@gmail.com>
2025-06-09 20:53:33 +0700

    complete the check function headers

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,86 +3,65 @@ package headers
 import (
 	"bytes"
 	"fmt"
-	"slices"
 	"strings"
-	"unicode"
 )
 
-const (
-	crlf           = "\r\n"
-	headerSeperate = ":"
-)
+const crlf = "\r\n"
 
 type Headers map[string]string
 
-var specialChars = []byte{
-	'!',
-	'#',
-	'$',
-	'%',
-	'&',
-	'\'',
-	'*',
-	'+',
-	'-',
-	'.',
-	'^',
-	'_',
-	'`',
-	'|',
-	'~',
+func NewHeaders() Headers {
+	return map[string]string{}
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	idx := bytes.Index(data, []byte(crlf))
-
 	if idx == -1 {
 		return 0, false, nil
 	}
 	if idx == 0 {
+		// the empty line
+		// headers are done, consume the CRLF
 		return 2, true, nil
 	}
 
-	headerLine := string(data[:idx])
-	parts := strings.SplitN(headerLine, headerSeperate, 2)
-	if len(parts) != 2 {
-		return 0, false, fmt.Errorf("invalid header format %s", headerLine)
-	}
-	rawKey := parts[0]
+	parts := bytes.SplitN(data[:idx], []byte(":"), 2)
+	key := strings.ToLower(string(parts[0]))
 
-	if strings.TrimSpace(rawKey) == "" {
-		return 0, false, fmt.Errorf("empty key string")
-	}
-	if strings.Contains(rawKey, " ") {
-		return 0, false, fmt.Errorf("invalid header format: space inside key")
+	if key != strings.TrimRight(key, " ") {
+		return 0, false, fmt.Errorf("invalid header name: %s", key)
 	}
 
-	key := strings.TrimSpace(rawKey)
-	value := strings.TrimSpace(parts[1])
-
-	if !validToken([]byte(key)) {
-		return 0, false, fmt.Errorf("invalid token header found: %s", key)
+	value := bytes.TrimSpace(parts[1])
+	key = strings.TrimSpace(key)
+	if !validTokens([]byte(key)) {
+		return 0, false, fmt.Errorf("invalid header token found: %s", key)
 	}
-	h.Set(key, value)
+	h.Set(key, string(value))
 	return idx + 2, false, nil
 }
 
 func (h Headers) Set(key, value string) {
 	key = strings.ToLower(key)
+	v, ok := h[key]
+	if ok {
+		value = strings.Join([]string{
+			v,
+			value,
+		}, ", ")
+	}
 	h[key] = value
 }
 
-func NewHeaders() Headers {
-	return make(map[string]string)
-}
+var tokenChars = []byte{'!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~'}
 
-func validToken(token []byte) bool {
-	for _, char := range token {
-		if slices.Contains(specialChars, char) || unicode.IsDigit(rune(char)) ||
-			unicode.IsLetter(rune(char)) {
-			return true
+// validTokens checks if the data contains only valid tokens
+// or characters that are allowed in a token
+func validTokens(data []byte) bool {
+	for _, c := range data {
+		if (c < 'A' || c > 'Z') && (c < 'a' || c > 'z') && (c < '0' || c > '9') && c != '-' {
+			return false
 		}
 	}
-
-	return false
+	return true
 }
